79d2e96765f34f6d4b157182b59b3b6f28191b1b
mattash <matt.ash@gmail.com>
2026-04-24 11:20:00 -0700

    Add ESM wrapper alongside existing CJS entry

---
+++ src/translit.mjs
@@ -0,0 +1,7 @@
+import cjsModule from './translit.js';
+
+export const transliterate = cjsModule.transliterate;
+export const transliterateWord = cjsModule.transliterateWord;
+export const transliterateByRules = cjsModule.transliterateByRules;
+export const loadDictionary = cjsModule.loadDictionary;
+export default cjsModule;
